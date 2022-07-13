bdef199d5d0f95adff6385974734f7016f4e76b6
lee <71934000+kdsnfkjdfs@users.noreply.github.com>
2022-07-13 12:28:45 +0900

    first c
    열심히 하자

---
+++ 반복문1.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+main(){
+	
+	int n=0;
+		
+	
+	while(5>n){
+		
+		printf("%d ",++n);
+		
+	}
+	printf("루프 종료");
+	
+	int i=30;
+	
+	while(i>=10){
+	
+		
+		printf("%d",i);
+		i-=2;
+		
+	}
+	
+}
---
+++ 반복문2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+main(){
+	int s;
+	sum=0;
+	
+	while(i<=10){
+		sum+=s;
+	}
+	printf("%d",sum);
+}
